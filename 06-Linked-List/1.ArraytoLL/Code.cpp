01ece6f7bdb2bbb680b39902e3891c563ae3eca5
Aparna Gupta <aparna060606@gmail.com>
2026-09-16 11:26:43 +0530

    Update Code.cpp

--- 06-Linked-List/1.ArraytoLL/Code.cpp
+++ 06-Linked-List/1.ArraytoLL/Code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+  public:
+    Node* arrayToList(vector<int>& arr) {
+        
+        Node* head = new Node(arr[0]);
+        Node* mover = head;
+        
+        for(int i=1; i<arr.size(); i++)
+        {
+            Node* temp = new Node(arr[i]);
+            mover->next = temp;
+            mover = mover->next;
+        }
+        return head;
+        
+    }
+};
